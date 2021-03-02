9b704e934834b1bb9f58822d722626c36f71abbe
enars <ejnar.sorensen@gmail.com>
2021-03-02 01:13:45 +0100

    Fix change cell type onClick, additional fix requried

--- astar.js
+++ astar.js
@@ -39,7 +39,6 @@ export function animate(fps) {
 function loop() {
     // calc elapsed time since last loop
     requestId = requestAnimationFrame(loop)
-    //console.log(frameCount++)
     now = Date.now()
     elapsed = now - then
 
@@ -52,21 +51,24 @@ function loop() {
 
     if (elapsed > fpsInterval) {
 
-        // Get ready for next frame by setting then=now, but also adjust for your
-        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
-        then = now - (elapsed % fpsInterval);
-
-        // Draw the next iteration and stop when finished
-        if (openSet.length > 0 && !stop)
+      // Get ready for next frame by setting then=now, but also adjust for your
+      // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+      then = now - (elapsed % fpsInterval);
+      
+      // Draw the next iteration and stop when finished
+      if (openSet.length > 0 && !stop) {
+        work()
+        
+        //if (!stop)
           draw()
-        else
-            window.cancelAnimationFrame(requestId);
-    }
+        
+      }
+      else
+        window.cancelAnimationFrame(requestId);
+  }
 }
 
-export function run() {
-  console.log(grid)
-
+export function clear() {
   start = grid[0][0] 
   end = grid[cols - 1][rows - 1] 
 
@@ -74,12 +76,16 @@ export function run() {
   path = []
   closedSet = []
   openSet = []
-  openSet.push(start);
+}
 
+export function run() {
+  clear()
+  openSet.push(start);
   animate(fps)
 }
 
 export function processNeighbors() {
+
   for (let x = 0; x < cols; x++) {
     for (let y = 0; y < rows; y++) {
       grid[x][y].addNeighbors(x, y, grid, cols, rows)
@@ -91,22 +97,39 @@ export function toggleOpenClosed(cell) {
   let isOpen = cell instanceof Open
   const x = cell.x
   const y = cell.y
-  const row = document.getElementById(`row-${cell.x}`)
-  
-  
+  const domCell = document.getElementById(`cell-${x}-${y}`)
+  const row = document.getElementById(`row-${x}`)
+
+  // Remove the old cell
+  domCell.remove()
   delete grid[x][y]
 
   if (isOpen) {
-    grid[x][y] = new Closed(x,y);
-    grid[x][y].neighbors = []
+    /**
+     * FIX: closed does not work, gets traversed through. Check if the element at location truly is changed, or see if neighbors update properly
+     */
     console.log('closed')
+    grid[x][y] = new Closed(x,y);
   } else {
     console.log('opened')
     grid[x][y] = new Open(x,y);
   }
- 
+  // Draw the newly created node
+  grid[x][y].draw()
+  
+  // Move it to the original location
+  const newDomCell = row.lastChild
 
+  /**
+   * Possible fix needed: check if is first??
+   */
+  const nextElement = document.getElementById(`cell-${x}-${y+1}`)
+  row.insertBefore(newDomCell, nextElement)
+
+  // Redetermine neighbors, clear moves, redraw grid
   processNeighbors()
+  clear()
+  draw()
 }
 
 // Setup grid, instantiate cells, count neighbors, set start and end, put start in openSet
@@ -132,54 +155,55 @@ export function setup(state) {
       grid[x][y].draw()
     }
   }
-  console.log(grid)
+
   processNeighbors()
 }
 
 // Draw and work algorithm
-function draw() {
+function work() {
   // Work
-    let winner = 0
-    for (let i = 0; i < openSet.length; i++) {
-      if (openSet[i].f < openSet[winner].f) {
-        winner = i
-      }
+  let winner = 0
+  for (let i = 0; i < openSet.length; i++) {
+    if (openSet[i].f < openSet[winner].f) {
+      winner = i
     }
-    let current = openSet[winner]
-    if (current === end) {
-      stop = true
-    }
-    console.log(current)
-    
-    removeFromArray(openSet, current);
-    closedSet.push(current)
+  }
+  let current = openSet[winner]
+  if (current === end) {
+    stop = true
+  }
+  
+  removeFromArray(openSet, current);
+  closedSet.push(current)
 
-    let neighbors = current.neighbors
-    for (let i=0; i< neighbors.length;i++) {
-      let neighbor = neighbors[i]
+  const neighbors = current.neighbors
+  for (let i=0; i< neighbors.length;i++) {
+    let neighbor = neighbors[i]
 
-      if (!closedSet.includes(neighbor)) {
-        const tempG = current.g + 1
+    if (!closedSet.includes(neighbor)) {
+      const tempG = current.g + 1
 
-        let improvement = false
-        if (openSet.includes(neighbor)) {
-          if (tempG < neighbor.g) {
-            neighbor.g = tempG
-            improvement = true
-          }
-        } else {
+      let improvement = false
+      if (openSet.includes(neighbor)) {
+        if (tempG < neighbor.g) {
           neighbor.g = tempG
-          openSet.push(neighbor)
           improvement = true
         }
-        if (improvement) {
-          neighbor.h = manhattanDist(neighbor,end)
-          neighbor.f = neighbor.g + neighbor.h
-          neighbor.previous = current
-        }
+      } else {
+        neighbor.g = tempG
+        openSet.push(neighbor)
+        improvement = true
+      }
+      if (improvement) {
+        neighbor.h = manhattanDist(neighbor,end)
+        neighbor.f = neighbor.g + neighbor.h
+        neighbor.previous = current
       }
     }
+  }
+}
 
+function draw() {
   // Draw
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
@@ -196,7 +220,8 @@ function draw() {
   }
 
   path = []
-  let temp = current
+  let temp = [...closedSet].pop();
+  if (temp){
   path.push(temp)
   while (temp.previous) {
     path.push(temp.previous)
@@ -205,5 +230,5 @@ function draw() {
 
   for (let i = 0; i < path.length; i++) {
     path[i].color('blue')
-  }
+  }}
 }
